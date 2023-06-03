3c2166734067f605de8071295e9c6c6605a82d8c
amonge20 <amonge20@jaumebalmes.net>
2023-06-03 11:11:48 +0200

    comentaris afegits i modificacions

--- Exercici1.py
+++ Exercici1.py
@@ -1,9 +1,17 @@
 import numpy as np
 
+# Funcio on crea la matrix i la retorna
 def create_matrix():
+    print("Creant la matriu...")
     matrix = np.diag(np.arange(50))
     return matrix
 
+# Funcio en que genera un fitxer que es dira exercici1.npy i guarda la la matriu generada en la funcio anterior
 def save_matrix():
     matrix = create_matrix()
+    print("Guardant la matriu en un fitxer exercici1.npy...")
     np.save('exercici1.npy', matrix)
+    print("Matriu guardada amb èxit!")
+
+# Quan executem el codi, executa la funcio save_matrix i afegira el fitxer exercici1.npy en el directori
+save_matrix()
