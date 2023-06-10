3972a277a56e8f488d0760efc77acfd7401ac636
amonge20 <amonge20@jaumebalmes.net>
2023-06-10 13:11:21 +0200

    main fet amb tots els exercicis fets

---
+++ main.py
@@ -0,0 +1,22 @@
+import Exercici1
+import Exercici2
+import Exercici3
+import Exercici4_Aitor
+
+print('Exercici 1')
+print(Exercici1.create_matrix())
+print(Exercici1.save_matrix)
+print('Exercici 2')
+print(Exercici2.matriu1())
+print(Exercici2.matriu2())
+print(Exercici2.matriu3())
+print('Exercici 3')
+print(Exercici3.mostrar_matriu())
+print(Exercici3.redimensionar_matriu())
+print(Exercici3.mostrar_matriu_redimensionada())
+print(Exercici3.trobar_valor_maxim())
+print(Exercici3.trobar_valor_minim())
+print('Exercici 4')
+print(Exercici4_Aitor.crear_matriu())
+print(Exercici4_Aitor.modificar_matriu_1())
+print(Exercici4_Aitor.modificar_matriu_2())
